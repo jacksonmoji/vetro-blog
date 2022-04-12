3dc7c61ab6339c60aaae3418dda8304b7cd9752c
Jackson Moji <jacksonmoji@gmail.com>
2022-04-13 00:33:09 +0200

    adding rating controller and model

---
+++ app/Http/Controllers/RatingController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Blog;
+
+
+class RatingController extends Controller
+{
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Blog $blog, Request $request)
+    {
+
+        $this->validate($request, [
+            'rating' => 'required',
+        ]);
+
+        $input = $request->all();
+
+        $blog->ratings()->create([
+            'rating' => $input['rating'],
+        ]);
+
+        return back();
+
+    }
+
+    // public function create(array $data)
+    // {
+    //   return Rating::create([
+    //     'rating' => $data['rating'],
+    //     'email' => $data['email'],
+    //     'password' => Hash::make($data['password'])
+    //   ]);
+    // }
+}
